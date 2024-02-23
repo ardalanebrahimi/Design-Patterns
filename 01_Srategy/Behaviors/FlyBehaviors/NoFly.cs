3e5d8b461c75c0fd2ba5e7ac1f92dcbb1aaad4c7
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2024-02-23 11:35:58 +0100

    add behavior classes

---
+++ 01_Srategy/Behaviors/FlyBehaviors/NoFly.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Srategy.Behaviors
+{
+    class NoFly
+    {
+        public string Run() => "I can't Fly";
+    }
+}
